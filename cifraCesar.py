e51f53738c9c583b303bebc1d758b4fc39b36f52
guiajalla <55240778+guiajalla@users.noreply.github.com>
2022-12-01 16:11:42 -0300

    fix: retirada entrada usuario

--- cifraCesar.py
+++ cifraCesar.py
@@ -1,9 +1,11 @@
+import sys
+
 MODO_CRIPTOGRAF = 1
 MODO_DESCRIPTOGRAF = 0
+CHAVE = 12
 
 def cifra(texto, chave, modo): #Desenvolvimento da Cifra de Cesar
-    alfabeto= "abcdefghijklmnopqrstuvwxyz"
-    texto = texto.lower() #Mantém o texto em minusculo, visto que o alfabeto utilizado, apenas trabalha com letras minusculas
+    alfabeto= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     new_txt = ''
     for c in texto: #Passa por cada caracter dentro do texto
         index = alfabeto.find(c) #Procura a posição desse caracter dentro da string alfabeto
@@ -17,50 +19,49 @@ def cifra(texto, chave, modo): #Desenvolvimento da Cifra de Cesar
 
 def verificaArquivo(): #Verifica se há um arquivo com esse nome, caso não tenha ele cria
     try:
-        f = open('arquivo_criptografado.txt')
+        f = open('arquivo_de_entrada.txt')
         f.close()
-        return True
     except:
-        return False
+        print("Criando arquivo para receber seu texto de entrada!")
+        f = open('arquivo_de_entrada.txt', 'w')
+        f.close()
+        sys.exit()
     
-def getInfo():
-    print('entrou')
+def getInfo(nome, op):
+    with open(nome, op) as f:
+        arquivo = f.readlines()
 
-def setInfo():
-    info = []
-    f = open('arquivo_criptografado.txt', 'w')
-    texto = input('\nDigite o que deseja criptografar: \n')
-    chave = int(input('\nDigite o número da chave para criptografar(1~26): \n'))
+    f.close()
+    return arquivo
 
-    txt_cifrado = cifra(texto, chave, MODO_CRIPTOGRAF)
-    info.append(txt_cifrado)
-    info.append(chave)
+def descriptografar():
+    nome = 'arquivo_criptografado.txt'
+    op = 'r'
+    arquivo = getInfo(nome, op)
 
-    for conteudo in info:
-        f.write(str(conteudo) + ';')
+    f = open('arquivo_descriptografado.txt', 'a')
+
+    for frase in arquivo:
+        descriptografado = cifra(frase, CHAVE, MODO_DESCRIPTOGRAF)
+        f.write(descriptografado)
     f.close()
 
-def interface():
-    print('----- Escolha o que deseja fazer -----')
-    print('1 - Criptografar')
-    print('2 - Descriptografar')
-    print('--------------------------------------')
-    op = input('Opcao: ')
-    match op:
-        case '1':
-            setInfo()
-        case '2':
-            if verificaArquivo() ==  False:
-                print('Você não possui um arquivo para descriptografar!\n')
-                return interface()
-            else:
-                print('Descriptografando arquivo')
-                getInfo()
-            
+def criptografar():
+    nome = 'arquivo_de_entrada.txt'
+    op = 'r'
+    arquivo = getInfo(nome, op)
 
+    f = open('arquivo_criptografado.txt', 'a') #Cria o arquivo caso não exista com a possibilidade de acrescentar
+
+    for frase in arquivo:
+        criptografado = cifra(frase, CHAVE, MODO_CRIPTOGRAF)
+        f.write(criptografado)
+    f.close()
 
 def main():
-    interface()
+    verificaArquivo()
+    criptografar()
+    descriptografar()
 
 if __name__ == "__main__":
     main()
